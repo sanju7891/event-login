855a9d5de4e54d07a307b3f86b5d32f26edc78bf
Sandeep Saraf <sandeepsaraf7891@gmail.com>
2023-03-23 03:06:36 +0530

    event add and delete feature

--- src/components/AddEvents.jsx
+++ src/components/AddEvents.jsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import { addDoc, collection, Timestamp } from 'firebase/firestore';
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
+import React, {useState} from 'react';
+import { toast } from 'react-toastify';
+import { db, storage } from '../firebaseConfig';
 
 export default function AddEvents(){
+    const [formData, setFormData] = useState({
+        title: "",
+        description: "",
+        image: "",
+        createdAt: Timestamp.now().toDate(),
+    })
+
+    const [progress, setProgress] = useState(0);
+
+    const handleChange=(e)=>{
+       setFormData({...formData,[e.target.name]:e.target.value});
+    }
+
+    const handleImageChange=(e)=>{
+        setFormData({...formData, image: e.target.files[0] });
+
+    }
+
+    const handlePublish = () =>{
+        if(!formData.title || !formData.description || !formData.image){
+            alert('please fill all fields');
+            return;
+        }
+
+        const storageRef = ref(storage,`/images/${Date.now()}${formData.image.name}`);
+
+        const uploadImage = uploadBytesResumable(storageRef, formData.image);
+
+        uploadImage.on("state_changed",
+        (snapshot)=> {
+            const progressPercent = Math.round((snapshot.bytesTranferred / snapshot.totalBytes) * 100);
+
+            setProgress(progressPercent);
+        },
+        (err) => {
+            console.log(err)
+        },
+        ()=>{
+            setFormData({
+                title: "",
+                description: "",
+                iamge: ""
+
+            });
+            getDownloadURL(uploadImage.snapshot.ref)
+            .then((url)=>{
+                const eventRef = collection(db, "events");
+                addDoc(eventRef,{
+                    title: formData.title,
+                    description: formData.description,
+                    iamgeUrl: url,
+                    createdAt: Timestamp.now().toDate(),
+                })
+                .then(()=>{
+                    toast("Event add successfully", {type: "success"});
+                    setProgress(0);
+                })
+                .catch(err=>{
+                    toast("Error adding event", {type: "error"});
+                })
+            }
+            )
+        }
+         );
+
+    }
      return (
-        <div>
-            Add Events
+        <div className="border p-3 mt-3 bg-light">
+            <h2>Create Event</h2>
+            <label>Title</label>
+            <input type="text" name="title" className='form-control' value={formData.title} onChange={(e)=>handleChange(e)} />
+
+            <label>Description</label>
+            <textarea type="text" name="description" className='form-control' value={formData.description} onChange={(e)=>handleChange(e)} />
+
+            <label>Image upload</label>
+            <input type="file" name="image" accept='image/*' className='form-control' onChange={(e)=>handleImageChange(e)} />
+
+
+            {
+                progress === 0 ? null : 
+                <div className="progress mt-2">
+                <div className='progress-bar progress-bar-striped mt-2 ' stlye={{width: `${progress}%`}}>
+                     {`uploading image ${progress}%`}
+                </div>
+
+            </div>
+            }
+            
+
+            <button className="btn btn-primary mt-2" onClick={handlePublish}>Publich</button>
         </div>
      )
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore';
-import {getStorage} from 'firebase/stroage';
+import {getStorage} from 'firebase/storage';
  
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDmmPrwy6U12gtOqZR2xfC0oHmr3rMY6XY",
-  authDomain: "event-app-af85c.firebaseapp.com",
-  projectId: "event-app-af85c",
-  storageBucket: "event-app-af85c.appspot.com",
-  messagingSenderId: "745935819509",
-  appId: "1:745935819509:web:02527f84c9b7070c44ebbc"
-};
+    apiKey: "AIzaSyDmmPrwy6U12gtOqZR2xfC0oHmr3rMY6XY",
+    authDomain: "event-app-af85c.firebaseapp.com",
+    projectId: "event-app-af85c",
+    storageBucket: "event-app-af85c.appspot.com",
+    messagingSenderId: "745935819509",
+    appId: "1:745935819509:web:02527f84c9b7070c44ebbc"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
